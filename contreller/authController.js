3e55080c16488d9e7f084de68fdf0e0c7d34f7c3
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-07-13 20:23:20 +0300

    User model created

--- contreller/authController.js
+++ contreller/authController.js
@@ -1,11 +1,20 @@
+const User = require('../models/User')
+
 module.exports.signup_get = (req, res) => {
     res.render('signup')
 }
-module.exports.signup_post = (req, res) => {
-    res.send('new signup')
+module.exports.signup_post = async (req, res) => {
     const {email, password} = req.body;
-    console.log(req.body);
-    console.log(email, password);
+
+    try {
+        const user = await User.create({email, password})
+        res.status(201).json(user);
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send('error, user not created')
+    }
+
 }
 module.exports.login_get = (req, res) => {
     res.render('login')
