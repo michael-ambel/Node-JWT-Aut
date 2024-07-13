bca78b3cdace56a1bb9974549115828f7a8ea871
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-07-13 12:15:53 +0300

    Testing Routes & Handling POST with Postman done!!

--- contreller/authController.js
+++ contreller/authController.js
@@ -3,10 +3,17 @@ module.exports.signup_get = (req, res) => {
 }
 module.exports.signup_post = (req, res) => {
     res.send('new signup')
+    const {email, password} = req.body;
+    console.log(req.body);
+    console.log(email, password);
 }
 module.exports.login_get = (req, res) => {
     res.render('login')
 }
 module.exports.login_post = (req, res) => {
     res.send('new login')
+    const {email, password} = req.body;
+    console.log(req.body);
+    console.log(email, password);
+    
 }
